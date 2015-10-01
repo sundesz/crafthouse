5315018bd6cb12ef18e1a2f89cdeefd3cfdfc9df
Sandesh Hyoju <sundesz@gmail.com>
2015-10-01 14:39:20 +0300

    day6 working with model

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -10,9 +10,7 @@
  */
 class jobActions extends sfActions {
 	public function executeIndex(sfWebRequest $request) {
-		$this->jobeet_jobs = Doctrine::getTable('JobeetJob')
-			->createQuery('a')
-			->execute();
+		$this->categories = Doctrine_Core::getTable('JobeetCategory')->getWithJobs();
 	}
 
 	public function executeShow(sfWebRequest $request) {
@@ -64,5 +62,13 @@ class jobActions extends sfActions {
 
 			$this->redirect('job/edit?id=' . $jobeet_job->getId());
 		}
+	}
+
+	public function getActiveJobs() {
+		$q = $this->createQuery('j')
+			->where('j.expires_at > ?', date('Y-m-d H:i:s', time()))
+			->orderBy('j.expires_at DESC');
+
+		return $q->execute();
 	}
 }
--- lib/model/doctrine/JobeetCategoryTable.class.php
+++ lib/model/doctrine/JobeetCategoryTable.class.php
@@ -1,5 +1,11 @@
 <?php
 
-class JobeetCategoryTable extends Doctrine_Table
-{
+class JobeetCategoryTable extends Doctrine_Table {
+	public function getWithJobs() {
+		$q = $this->createQuery('c')
+			->leftJoin('c.JobeetJobs j')
+			->where('j.expires_at > ?', date('Y-m-d H:i:s', time()));
+
+		return $q->execute();
+	}
 }
