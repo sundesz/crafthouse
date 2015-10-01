8244960a42fb674997d0e844d31e3bd5cf58d162
Sandesh Hyoju <sundesz@gmail.com>
2015-10-01 14:25:56 +0300

    day 5: The routing

--- apps/frontend/modules/job/templates/indexSuccess.php
+++ apps/frontend/modules/job/templates/indexSuccess.php
@@ -4,8 +4,7 @@
       <tr class="<?php echo fmod($i, 2) ? 'even' : 'odd';?>">
         <td class="location"><?php echo $job->getLocation();?></td>
         <td class="position">
-          <a href="<?php echo url_for('job/show?id=' . $job->getId());?>">
-            <?php echo $job->getPosition();?>
+          <?php echo link_to($job->getPosition(), 'job_show_user', $job);?>
           </a>
         </td>
         <td class="company"><?php echo $job->getCompany();?></td>
